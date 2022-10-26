63d6831e715dca9cd3b02fc09aa5af1e985d5036
magmar33 <magmar33@hotmail.com>
2022-10-26 17:43:52 -0500

    first commit

---
+++ app/src/main/java/com/narvasoft/bd/User.java
@@ -0,0 +1,29 @@
+package com.narvasoft.bd;
+
+public class User {
+    private String names, user, password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+}
